fa955ea03f622f395dbb16226b90b74000d88791
iqorAdmin <josedavids123@live.com>
2025-08-08 18:38:15 -0500

    se arregla error general en productos, se inicial con el productoDetails

--- js/funciones.js
+++ js/funciones.js
@@ -1,25 +1,29 @@
 const productosPorPagina = 12;
 let productos = [];
 let paginaActual = 1;
+const descuento = 50000;
+let producto;
 
-function mostrarPagina(pagina){
+function mostrarPagina(pagina) {
     const contenedor = document.getElementById("contenedorProductos");
-    contenedor.innerHTML="";
+    contenedor.innerHTML = "";
     const inicio = (pagina - 1) * productosPorPagina;
     const fin = inicio + productosPorPagina;
     const productosPagina = productos.slice(inicio, fin);
 
     productosPagina.forEach(p => {
+
         const div = document.createElement("div");
         div.className = "col-4";
-        div.innerHTML=`
+        div.innerHTML = `
         <a class="aProductos" href="home/productos/detallesProducto/${p.id}">
-        <img class="imagenProductos" src="${p.imageUrl}" alt="${p.nombre}">
+        <img class="imagenProductos" src="${p.imageUrl[0]}" alt="${p.nombre}">
         </a>
         <h4>${p.nombre}</h4>
         <div class="rating">
         ${generarEstrellas(p.rating)}
         </div>
+        <p style="text-decoration: line-through !important; color: red !important;">$${p.precio + descuento}<p>
         <p>$${p.precio}</p>        
         `;
 
@@ -28,39 +32,41 @@ function mostrarPagina(pagina){
     paginaActual = pagina;
 }
 
-function generarEstrellas(rating){
-    let estrellas ="";
+function generarEstrellas(rating) {
+    let estrellas = "";
     const enteras = Math.floor(rating);
     const media = rating % 1 >= 0.5;
 
-    for(let i = 0; i < enteras; i++) estrellas += `<i class="fa-solid f-star"></i>`;
-    if(media) estrellas += `<i class="fa-solid fa-star-half-stroke"></i>`;
-    const vacias = 5 - enteras - (media ? 1: 0);
-    for(let i = 0; i< vacias;i++) estrellas += `<i class="fa-regular fa-star"></i>`;
+    for (let i = 0; i < enteras; i++) estrellas += `<i class="fa-solid fa-star"></i>`;
+    if (media) estrellas += `<i class="fa-solid fa-star-half-stroke" ></i>`;
+    const vacias = 5 - enteras - (media ? 1 : 0);
+    for (let i = 0; i < vacias; i++) estrellas += `<i class="fa-solid fa-star" style="color: grey;" ></i>`;
 
     return estrellas;
 }
 
-function crearPaginacion(){
+function crearPaginacion() {
     const contenedorPag = document.getElementById("paginacion");
-    contenedorPag.innerHTML="";
+    contenedorPag.innerHTML = "";
 
     const totalPag = Math.ceil(productos.length / productosPorPagina);
     for (let i = 0; i < totalPag; i++) {
         const span = document.createElement("span");
         span.textContent = i + 1;
-        if(i === paginaActual) span.classList.add("activo");
-        span.addEventListener("click", () =>{
+        if (i === paginaActual) span.classList.add("activo");
+        span.addEventListener("click", () => {
             mostrarPagina(i);
             crearPaginacion();
         });
         contenedorPag.appendChild(span);
-        
+
     }
 }
 
 
-
+function mostrarProducto(producto){
+    
+}
 
 
 
@@ -70,33 +76,58 @@ function crearPaginacion(){
 
 
 function listarProductos() {
-fetch("http://localhost:1000/home/productos", {
-    method:"GET",
-    headers:{
-        "Content-Type":"application/json"
-    }
-})
-.then(response =>{
-    if(!response.ok){
-        console.error("Error al obetener productos");
-    }
-   
-    return response.json();
-})
-.then(data =>{
-        console.log(data);
-      productos = data.dtos;  
-    
-    
-    mostrarPagina(1);
-    crearPaginacion();
-   
-    
-})
-.catch(error => {
-    console.error("Error al traer los productos:", error);
-});
+    fetch("http://localhost:1000/home/productos", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.error("Error al obetener productos");
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            productos = data.dtos;
 
 
+            mostrarPagina(1);
+            crearPaginacion();
+
+
+        })
+        .catch(error => {
+            console.error("Error al traer los productos:", error);
+        });
+
+
+}
+function redirigirProducto(id) {
+    fetch(`http://localhost:1000/home/productos/detallesProducto/${id}`, {
+
+
+        method: "GET",
+        headers: {
+            "Content-type":"application/json"
+
+        }
+    }
+    )
+    .then(response => {
+        if(!response.ok){
+            console.error("Error al obtener datos");
+        }
+        return response.json();
+    })
+    .then(data => {
+        producto = data;
+        mostrarProducto(producto);
+    })
+    .catch(error => { 
+        console.log("Error al mostrar el producto: ", error);
+    });
 }
 listarProductos();
